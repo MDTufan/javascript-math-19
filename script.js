a8e66d48f7d9e2b82a8022083018ed3f969846eb
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-06-24 18:16:31 -0700

    Add files via upload
    math objects

---
+++ script.js
@@ -0,0 +1,105 @@
+let x = 3.14;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x, 2);
+//x = Math.sqrt(x);
+//x = Math.abs(x);
+//maximum = Math.max(x, y, z);
+//minimum = Math.min(x, y, z);
+//x = Math.PI;
+
+console.log(x);
+
+
+
+
+
+// var num = "20";
+// num = num.toString();
+// console.log(typeof num);
+
+// var number = 20;
+// console.log(typeof number);
+
+
+ 
+
+// Arithmetic operator -> +, -, *, / , %, ++, --, ** (exponentiation)
+// Assignment operator -> =, +=, -=, *=, /=, %=
+
+// var num1 = parseInt(prompt("Enter first number : "));
+// var num2 = parseInt(prompt("Enter second number : "));
+// var lineBreak = "<br/>"
+// var lineBreak = "<br/>";
+
+// var result = num1 + num2
+// var result = num1 + num2;
+// document.write("The sum is : " + result + lineBreak);
+
+// result = num1 - num2
+// result = num1 - num2;
+// document.write("The sub is : " + result + lineBreak);
+
+// result = num1 * num2
+// result = num1 * num2;
+// document.write("The multiplication is : " + result + lineBreak);
+
+// result = num1 / num2
+// result = num1 / num2;
+// document.write("The division is : " + result + lineBreak);
+
+// result = num1 % num2
+// document.write("The remainder is : " + result + lineBreak);
+// result = num1 % num2;
+// document.write("The remainder is : " + result + lineBreak);
+
+
+
+// var numbers = [10,20,30,40];
+// console.log(numbers)
+// numbers.forEach(function(x,index,arr){
+//     arr[index] = x+5;
+// })
+// console.log(numbers)
+
+// var numbers = [10, 20, 30, 40];
+// console.log(numbers);
+// numbers.forEach(function (x, index, arr) {
+//   arr[index] = x + 5;
+// });
+// console.log(numbers);
+
+
+// var date = new Date();
+// console.log(date)
+// console.log(date);
+
+// var year = date.getFullYear();
+// console.log(year)
+// console.log(year);
+
+// var month = date.getMonth();
+// console.log(month)
+// console.log(month);
+
+// var currentDate = date.getDate();
+// console.log(currentDate)
+// console.log(currentDate);
+
+// var currentDay = date.getDay();
+// console.log(currentDay)
+// console.log(currentDay);
+
+// var currentHour = date.getHours();
+// console.log(currentHour)
+// console.log(currentHour);
+
+// var currentMinutes = date.getMinutes();
+// console.log(currentMinutes)
+// console.log(currentMinutes);
